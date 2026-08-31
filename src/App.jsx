89ba6041f21725312437a129b9894ce5c4c0c04c
SaifProject <ashrafulsaif23@gmail.com>
2026-08-31 11:13:36 +0600

    Learn React state, events, API fetching and Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 import './Counter.jsx'
 import Counter from './Counter.jsx'
 import Batsman from './Batsman.jsx'
+import { Suspense } from 'react'
+import Users from './Users.jsx'
+
+const fetchUsers =fetch('https://jsonplaceholder.typicode.com/users')
+ .then(res => res.json()) ;
 
 function App(){
 
@@ -12,17 +17,16 @@ function App(){
  return (
   <>
   <h3> Vite + React </h3>
+  <Suspense fallback={<h3> Loading....</h3>}>
+   <Users fetchUsers={fetchUsers}> </Users>
+ </Suspense>
 
   <Batsman></Batsman>
   <Counter> </Counter>
   {/* <button onclick="handleClick()"> Click Me</button> */}
   <button onClick={handleClick}> Click me</button>
   {/* uasing arry func */}
-  <button onClick={() => alert('Click 2')} > Click me 2</button>
-
-
-
-    
+  <button onClick={() => alert('Click 2')} > Click me 2</button>    
   </>
  )
 }
